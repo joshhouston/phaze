86d5a69cee5f7e11b84b4ebc49c65fe6c1af5899
joshhouston <jjhouston24@gmail.com>
2020-01-07 18:01:19 -0600

    boiler plate

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Canvas, useFrame } from 'react-three-fiber'
+import './App.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Thing() {
+    const ref = useRef()
+    useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01))
+    return (
+      <mesh
+        ref={ref}
+        onClick={e => console.log('click')}
+        onPointerOver={e => console.log('hover')}
+        onPointerOut={e => console.log('unhover')}>
+        <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+        <meshNormalMaterial attach="material" />
+      </mesh>
+    )
+  }
+  
+
+ReactDOM.render(
+    <Canvas>
+        <Thing />
+    </Canvas>,
+ document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
